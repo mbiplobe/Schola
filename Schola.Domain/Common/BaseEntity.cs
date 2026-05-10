cbc9a2462c875a9eba218e173f851b9bf4215b6b
Mujahid Islam <mbiplobe@gmail.com>
2026-05-10 08:00:52 +0600

    use shred file

--- Schola.Domain/Common/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace Schola.Domain.Common;
-
-public abstract class BaseEntity
-{
-    public long Id { get; protected set; }
-
-    public Guid Uuid { get; protected set; } = Guid.NewGuid();
-}
